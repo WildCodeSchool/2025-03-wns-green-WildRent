89f27bd5314200763e75c5403f7ebf0dd23f8fe3
cindy grisez <grisezcindy@gmail.com>
2025-07-31 11:56:01 +0200

    added to entities, modify to tsconfig for activate decorators and moove graphql dependencie to the backend package.js

---
+++ backend/src/entities/Status.ts
@@ -0,0 +1,29 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+} from "typeorm";
+
+import { Booking } from "./Booking";
+
+@Entity()
+@ObjectType()
+export class Status extends BaseEntity {
+    
+    @Field()
+    @PrimaryGeneratedColumn()
+    id!: number;
+    
+    @Field()
+    @Column({ length: 20 })
+    statusName!: string;
+    
+    @OneToMany(() => Booking, (booking) => booking.status)
+    @Field(() => [Booking])
+    bookings!: Booking[];
+    
+
+}
