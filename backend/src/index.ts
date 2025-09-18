27d80d35b93a0eb1a6cf23e134e1a54f2a411d46
Clement Thorez <clementthorez@Air-de-Clement.lan>
2025-09-18 11:11:02 +0200

    fix type to function login (non résolu)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,20 @@
 
 import "reflect-metadata";
 import * as dotenv from "dotenv";
+dotenv.config();
 import { dataSource } from "./config/db";
 import { buildSchema } from "type-graphql";
 import { ApolloServer }  from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import UserResolver from "./resolvers/UserResolver";
 import AuthResolver from "./resolvers/AuthResolver";
+import { AnonContext, AuthContext } from "./types/types";
 
 type Query = {
   _empty: String
 }
 
-dotenv.config();
+console.log("DEBUG JWT_SECRET:", process.env.JWT_SECRET);
 
 async function startServer() {
   await dataSource.initialize();
@@ -21,8 +23,15 @@ async function startServer() {
   })
   const apolloServer = new ApolloServer({ schema });
   const { url } = await startStandaloneServer(apolloServer, {
-    listen: {port: 4200}}
-);
+    listen: {port: 4200},
+    context: async({req, res}) => {
+      const context : AnonContext | AuthContext = {
+        req, 
+        res
+      };
+      return context
+    }
+  });
   console.log("✅ Server started on " + url);
 };
 startServer();
--- backend/src/resolvers/AuthResolver.ts
+++ backend/src/resolvers/AuthResolver.ts
@@ -1,5 +1,32 @@
-import { Arg, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { GraphQLJSONObject } from "graphql-scalars";
+import argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { UserService } from "../services/user.service";
+import { AnonContext, UserToken } from "../types/types";
+import { User } from "../entities/User";
 
+function getUserTokenContent(user: User): UserToken{
+  return {
+    mail: user.email,
+    firstName: user.firstname,
+    lastName: user.lastname
+  }
+};
+
+function getUserPublicProfil(user: User) {
+  return {
+    name: user.firstname,
+  }
+};
+
+function setCookie(context: AnonContext, tokenName: string, tokenValue: string) {
+  context.res.setHeader(
+    "Set-Cookie",
+    `${tokenName}=${tokenValue};secure;httpOnly;SameSite=strict`
+  );
+};
 
 @InputType()
 export class LoginInput {
@@ -10,10 +37,38 @@ export class LoginInput {
   password!: string;
 }
 
+@ObjectType()
+export class UserLogin {
+  @Field()
+  name!: string;
+}
+
 @Resolver()
 export default class AuthResolver {
-    @Mutation(() => String)
-    async login(@Arg("data") data: LoginInput): Promise<string> {
-        return "login ok"
+  private userService: UserService;
+
+  constructor(userService: UserService = new UserService()) {
+    this.userService = userService;
+  }
+    @Mutation(() => UserLogin)
+    async login(@Arg("data") data: LoginInput, @Ctx() context: AnonContext){
+      try {
+        if(!process.env.JWT_SECRET) throw new Error("Clé secrète manquante");
+
+        const user = await this.userService.findByMail(data.mail);
+        if(!user) throw new Error("Utilisateur introuvable");
+
+        const isValid = await argon2.verify(user.password, data.password);
+        if(!isValid) throw new Error("Mot de passe incorrecte");
+
+        const token = jwt.sign(getUserTokenContent(user), process.env.JWT_SECRET);
+
+        setCookie(context, "WildRentAuthToken", token);
+
+        return getUserPublicProfil(user);
+
+      } catch (err: any) {
+        throw new Error(`Failed to login: ${err.message}`);
+      }
     }
 };
