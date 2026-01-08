5c17f122c5100f3b0d87f8ffbe88eb7b36e1c785
alicepgrd <pougeardalice@gmail.com>
2026-01-08 10:09:45 +0100

    Add class-validator to DTOs for input validation

--- backend/src/dtos/booking-products.dto.ts
+++ backend/src/dtos/booking-products.dto.ts
@@ -1,19 +1,27 @@
 import { Field, InputType, ID, Int } from "type-graphql";
+import { IsInt, Min, IsOptional } from "class-validator";
 
 @InputType()
 export class CreateBookingProductsInput {
   @Field(() => ID)
+  @IsInt()
   bookingId!: number;
 
   @Field(() => ID)
+  @IsInt()
   productId!: number;
 
   @Field(() => Int)
+  @IsInt()
+  @Min(1)
   productQuantity!: number;
 }
 
 @InputType()
 export class UpdateBookingProductsInput {
   @Field(() => Int, { nullable: true })
+  @IsInt()
+  @IsOptional()
+  @Min(1)
   productQuantity?: number;
 }
