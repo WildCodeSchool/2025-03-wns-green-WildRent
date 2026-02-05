778e83b670ed6f65fbf0bbdffdf5455407653c59
Clément THOREZ <cthorez@selectour.fr>
2026-02-05 18:09:06 +0100

    create card component and finalise style card

---
+++ frontend/src/components/CategoryCard/ProductCard.tsx
@@ -0,0 +1,24 @@
+type ProductCardProps = {
+    title: string; 
+    brand: string; 
+    price: number;
+    image: string; 
+};
+
+export const ProductCard = ({ title, brand, price, image }: ProductCardProps) => {
+    return(
+        <div className="w-xs m-5">
+                <img src={image} alt="" className="w-full border-x-1 border-t-1 rounded-t-2xl border-[#87a700]"/>
+                <div className="w-full px-5 py-3 rounded-b-2xl bg-[#31380d]">
+                    <div className="mb-5">
+                        <h1 className="text-[#87a700] font-[family-name:var(--font-text)]"><strong>{title}</strong></h1>
+                        <h2 className="text-[#fdffe9] font-[family-name:var(--font-text)]">{brand}</h2>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                        <p className="text-[#acaf91] font-[family-name:var(--font-text)]">Prix par jour</p>
+                        <p className="text-[#87a700] font-[family-name:var(--font-text)]"><strong>{price}€</strong></p>
+                    </div>
+                </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/CategoryCard/ProductsGrid.tsx
@@ -0,0 +1,55 @@
+import { ProductCard } from "./ProductCard";
+
+type Product = {
+    title: string; 
+    brand: string; 
+    image: string; 
+    price: number; 
+};
+
+const products: Product[] = [
+    {
+        title: "Bâton de ski", 
+        brand: "Rossignol", 
+        image: "/images/baton.png",
+        price: 10
+    },
+    {
+        title: "Chaussure de ski", 
+        brand: "Lange", 
+        image: "/images/boot1.png",
+        price: 18
+    },
+    {
+        title: "Chaussure de ski", 
+        brand: "Rossignol", 
+        image: "/images/boot2.png",
+        price: 18
+    },
+    {
+        title: "Ski", 
+        brand: "Rossignol", 
+        image: "/images/ski.png",
+        price: 35
+    },
+    {
+        title: "Planche de snowboard", 
+        brand: "Rossignol", 
+        image: "/images/snow1.png",
+        price: 31
+    },
+    {
+        title: "Planche de snowboard", 
+        brand: "Burton", 
+        image: "/images/snow2.png",
+        price: 31
+    }
+];
+
+export const ProductsGrid = () => {
+    return(
+        <div>
+            {products.map((product)) => ()}
+        </div>
+    )
+}
