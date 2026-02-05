2becd3e06e0168d944cb26e83e9a927c9aea3cb5
Clément THOREZ <cthorez@selectour.fr>
2026-02-05 11:27:55 +0100

    initialize react-router with RouterProvider + apolloClient for ApolloProvider (using graphQL with react)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { CategoryGrid } from './components/CategoryCard/CategoryGrid.tsx'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+import { Layout } from './pages/Layout.tsx'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:5173/" }),
+  cache: new InMemoryCache(),
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>, 
+    children : 
+    [
+      { index: true, element: <CategoryGrid/> },
+      { path: "products", element: <CategoryGrid/> }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+      {/* <App /> */}
   </StrictMode>,
 )
