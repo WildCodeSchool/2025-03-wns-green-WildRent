646a88b8d3645af16889c3dd0abbc9ca176cc932
Julien Murat CAKIR <114389896+ROYPIDO@users.noreply.github.com>
2026-03-04 16:46:30 +0100

    feat(home): create and integrate RentalProcess component (#55)

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,10 +1,12 @@
 import { CategoryGrid } from "../../components/CategoryGrid";
+import { RentalProcess } from "../../components/RentalProcess";
 import Hero from "./Hero";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
+      <RentalProcess />
       <CategoryGrid />
     </>
   );
